425ee472bdc3f69f097313906bdd226d4a21b5ce
Brennan Heisler <learn@heislercreative.com>
2018-03-20 02:37:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    if 
+    user = User.find_by(username: params[:username], password: params[:password])
   end
 
   get "/failure" do
